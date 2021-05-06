949b9bef1f14e6ee9c5a2ece6465ed41db6b6356
Jon Vaillant <hi@jonvaillant.com>
2021-05-06 16:40:52 +0800

    Progress on map

---
+++ Client/Shared/Constants.cs
@@ -0,0 +1,12 @@
+namespace BlazorDemo.Client.Shared.Constants
+{
+    public class Constants
+    {
+        public static class MapLocations {
+            public static string DeepWaterChannel = "DeepWaterChannel";
+            public static string InnerHarbour = "InnerHarbour";
+            public static string ParmeliaChannel = "ParmeliaChannel";
+            public static string OuterHarbour = "OuterHarbour";
+        };
+    }
+}
---
+++ Client/Shared/Models/MapPointModel.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace BlazorDemo.Client.Shared.Models
+{
+    public class MapPointModel
+    {
+        public string Identifier { get; set; }
+        
+        public string Label { get; set; }
+
+        public List<int> Coordinates { get; set; }
+
+    }
+}
