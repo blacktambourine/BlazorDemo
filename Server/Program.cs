375c115d79502d46e40d838e688c35f6abe045a3
Patrick.Stysiak <patrick.stysiak@empired.com>
2021-08-24 09:02:20 +0800

    Setup for deploying to Azure WebApp, KeyVault, SignalR services

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Azure.Identity;
+using Microsoft.Extensions.Configuration;
+using System;
 
 namespace BlazorDemo.Server
 {
@@ -14,6 +17,11 @@ namespace BlazorDemo.Server
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
+                    config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
+                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
